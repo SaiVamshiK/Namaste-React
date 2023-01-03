951299a0f6273fc7b85fa2580793b81881e44c6e
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-03 19:43:10 +0530

    LESSON 2 : Boiler plate code

---
+++ Lesson 2/App.js
@@ -0,0 +1,23 @@
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+  },
+  "Hello World 1"
+);
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "heading2",
+  },
+  "Hello World 2"
+);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
