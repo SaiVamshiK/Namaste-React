24dd4db724b2a6a633126949a7a3d938fa62793b
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-06 17:54:28 +0530

    LESSON 3 : using createElement directly

--- Lesson 3/App.js
+++ Lesson 3/App.js
@@ -1,15 +1,15 @@
-import React from "react";
+import { createElement as ce} from "react";
 import ReactDOM from "react-dom/client";
-const heading1 = React.createElement('h1',{
+const heading1 = ce('h1',{
     id : 'heading1',
     key : 'key1'
 },'Heading 1');
-const heading2 = React.createElement('h1',{
+const heading2 = ce('h1',{
     id : 'heading2',
     key : 'key2'
 },'Heading 2');
 console.log(heading1);
-const container = React.createElement('div',{
+const container = ce('div',{
     id : 'container'
 },[heading1,heading2]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
