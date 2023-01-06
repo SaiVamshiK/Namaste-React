6c780135fd41929768fcd63a90ddb8191166701d
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-06 21:29:13 +0530

    LESSON 3 : creating nested header element with JSX

--- Lesson 3/Assignment/App.js
+++ Lesson 3/Assignment/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement('h1',{
-    key : 'key1'
-},'Heading 1');
-const heading2 = React.createElement('h1',{
-    key : 'key2'
-},'Heading 2');
-const heading3 = React.createElement('h1',{
-    key : 'key3'
-},'Heading 3');
-const container = React.createElement('div',{
-    class : 'title'
-},[heading1,heading2,heading3]);
+// const heading1 = React.createElement('h1',{
+//     key : 'key1'
+// },'Heading 1');
+// const heading2 = React.createElement('h1',{
+//     key : 'key2'
+// },'Heading 2');
+// const heading3 = React.createElement('h1',{
+//     key : 'key3'
+// },'Heading 3');
+// const container = React.createElement('div',{
+//     class : 'title'
+// },[heading1,heading2,heading3]);
+const container = (
+    <>
+        <div className ='title'>
+            <h1>Heading1</h1>
+            <h1>Heading2</h1>
+            <h1>Heading3</h1>
+        </div>
+    </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container)
+root.render(container);
