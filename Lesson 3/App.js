d1e2ee7588d8cac0c0be4226ca9fe1b8285d36b6
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-06 18:00:44 +0530

    LESSON 3 : adding JSX

--- Lesson 3/App.js
+++ Lesson 3/App.js
@@ -1,17 +1,36 @@
-import { createElement as ce} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-const heading1 = ce('h1',{
-    id : 'heading1',
-    key : 'key1'
-},'Heading 1');
-const heading2 = ce('h1',{
-    id : 'heading2',
-    key : 'key2'
-},'Heading 2');
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+    key: "key1",
+  },
+  "Heading 1"
+);
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "heading2",
+    key: "key2",
+  },
+  "Heading 2"
+);
 console.log(heading1);
-const container = ce('div',{
-    id : 'container'
-},[heading1,heading2]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log('hello');
+const heading3 = (
+  <>
+    <h1 key="key3">Heading3</h1>
+    <h1 key="key4">Heading4</h1>
+  </>
+);
+console.log(heading3);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2,heading3]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("hello");
 root.render(container);
