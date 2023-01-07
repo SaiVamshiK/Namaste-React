1c0240ade927021624fac72f601166218dac9f68
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-07 11:23:43 +0530

    LESSON 4 : Starting with Food Villa App

--- Lesson 4/App.js
+++ Lesson 4/App.js
@@ -1,12 +1,22 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
+
+const Title = () => {
+  return (
+    <h1 id="title" key="h2">
+      Food Villa
+    </h1>
+  );
+};
+
 const HeadingComponent = () => {
   return (
     <>
+      <Title />
       <h1>Heading</h1>
     </>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<HeadingComponent />);
