71e8dd4ff48e2e51c323a3ea0a85d8eacee8ec50
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-07 12:21:04 +0530

    LESSON 4 : adding a Restaurant Card

--- Lesson 4/App.js
+++ Lesson 4/App.js
@@ -3,14 +3,17 @@ import React from "react";
 
 const Title = () => {
   return (
-    <img className="logo" alt="logo" src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"/>
+    <img
+      className="logo"
+      alt="logo"
+      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+    />
   );
 };
 
 const styleObject = {
-    color : "blue"
+  color: "blue",
 };
-
 
 const Header = () => {
   return (
@@ -18,39 +21,53 @@ const Header = () => {
       <Title />
       <div className="nav-items">
         <ul>
-            <li style={styleObject}>Home </li>
-            <li style={styleObject}>About </li>
-            <li style={styleObject}>Contact Us </li>
-            <li style={styleObject}>Cart </li>
+          <li style={styleObject}>Home </li>
+          <li style={styleObject}>About </li>
+          <li style={styleObject}>Contact Us </li>
+          <li style={styleObject}>Cart </li>
         </ul>
       </div>
     </div>
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="card">
+      <img
+        alt="card"
+        src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2VyJTIwa2luZ3xlbnwwfHwwfHw%3D&w=1000&q=80"
+      />
+      <h2>Burger King</h2>
+      <h3>Burgers, American</h3>
+      <h4>4.2 stars</h4>
+    </div>
+  );
+};
+
 const Body = () => {
-    return (
-        <>
-        <h4>Body</h4>
-        </>
-    );
-}
+  return (
+    <div>
+        <RestaurantCard/>
+    </div>
+  );
+};
 
 const Footer = () => {
-    return (
-        <>
-        <h4>Footer</h4>
-        </>
-    );
-}
+  return (
+    <>
+      <h4>Footer</h4>
+    </>
+  );
+};
 
 const AppLayout = () => {
   return (
     <React.Fragment>
-        {/* Header
-            - Logo
-            - Navbar Items (Right Side)
-            - Cart 
+      {/* Header
+            - Logo => Title
+            - Navbar Items (Right Side) => Header
+            - Cart => Header
         Body
             - Search Bar
             - List of Restaurants (Restaurants Cards)
@@ -62,9 +79,9 @@ const AppLayout = () => {
         Footer
             - Reference Links
             - CopyRights */}
-        <Header/> 
-        <Body/>
-        <Footer/>
+      <Header />
+      <Body />
+      <Footer />
     </React.Fragment>
   );
 };
