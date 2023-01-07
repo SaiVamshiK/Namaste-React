0d7e28296b7febf00b7b0319f00e3fb344306226
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-07 19:44:09 +0530

    LESSON 4 : basic Navbar Header Component with styling

---
+++ Lesson 4/Assignment/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+      />
+    </a>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div>
+      <h1>Body</h1>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
