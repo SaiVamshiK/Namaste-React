4912a69f133e5abf1060ea793202344edc4a5b66
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-14 10:10:13 +0530

    LESSON 5 : about hooks

--- Lesson 5/src/components/Body.js
+++ Lesson 5/src/components/Body.js
@@ -1,15 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../config";
+import { useState } from "react";
+
+// What is state
+// Whenever we create a local variables we use state variables.
+// State variables are created using useState hooks.
+
+// What is Hooks
+// Hooks is just a JS function.
+// we get useState from react
+
+// What is useState
+// The function is this useState hook is to create state variables.
+// useState() returns the array
+// Elem 0 is the variable name
+//
 
 const Body = () => {
-  let searchTxt = "KFC";
+  const [searchTxt] = useState("KFC");
+  // searchTxt is the local state variable
+  // we can use the searchTxt variable as a normal variable.
   return (
     <>
       <div className="search-container">
         <input type="text" className="search-input" placeholder="Search" value={searchTxt} onChange={(e) => {
-          console.log(e.target.value);
-          console.log(searchTxt);
-          searchTxt = e.target.value;
+          console.log(e.target.value)
         }}/>
         <button className="search-btn">Search</button>
         {/* We will not be able to edit the above input KFC value though */}
