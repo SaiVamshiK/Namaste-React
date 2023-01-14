748c32862b80a1d5afd977de162fc3239212b0a1
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-14 08:43:41 +0530

    LESSON 5 : input box in JSX with default value

--- Lesson 5/src/components/Body.js
+++ Lesson 5/src/components/Body.js
@@ -2,11 +2,13 @@ import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../config";
 
 const Body = () => {
+  const searchTxt = "KFC";
   return (
     <>
       <div className="search-container">
-        <input type="text" className="search-input" placeholder="Search" value=""/>
+        <input type="text" className="search-input" placeholder="Search" value={searchTxt}/>
         <button className="search-btn">Search</button>
+        {/* We will not be able to edit the above input KFC value though */}
       </div>
       <div className="restaurant-list">
         {restaurantList.map((restaurant, index) => (
