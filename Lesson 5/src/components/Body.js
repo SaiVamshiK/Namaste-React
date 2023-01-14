2d9cef11641a6327dca5274a672fce188f156ef0
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-14 10:13:54 +0530

    LESSON 5 : now the input box works

--- Lesson 5/src/components/Body.js
+++ Lesson 5/src/components/Body.js
@@ -14,17 +14,20 @@ import { useState } from "react";
 // The function is this useState hook is to create state variables.
 // useState() returns the array
 // Elem 0 is the variable name
-//
+// Elem 1 is the function
 
 const Body = () => {
-  const [searchTxt] = useState("KFC");
+  let [searchTxt,setSearchText] = useState("KFC");
   // searchTxt is the local state variable
   // we can use the searchTxt variable as a normal variable.
+  // We cannot modify the searchTxt local state variable directly like searchTxt = e.target.value
+  // We modify the variable only using a function
   return (
     <>
       <div className="search-container">
         <input type="text" className="search-input" placeholder="Search" value={searchTxt} onChange={(e) => {
-          console.log(e.target.value)
+          console.log(e.target.value);
+          setSearchText(e.target.value);
         }}/>
         <button className="search-btn">Search</button>
         {/* We will not be able to edit the above input KFC value though */}
