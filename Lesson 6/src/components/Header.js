40b5bc822e4a6671a038486625f7ec610a3c556c
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-19 10:19:46 +0530

    LESSON 6 : toggling title

--- Lesson 6/src/components/Header.js
+++ Lesson 6/src/components/Header.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import Title from "./Title";
 export const Header = () => {
+  const [title,setTitle] = useState('Food Villa');
   return (
     <div className="header">
       <Title />
+      <h1>{title}</h1>
+      <button onClick={() => {
+        if(title === 'Food Villa'){
+          setTitle("Food Villa Updated")
+        }else{
+          setTitle("Food Villa")
+        }
+      }}>Change title</button>
       <div className="nav-items">
         <ul>
           <li>Home</li>
