9a3851b89be07ced6aacdf28059275bf96cb9bd7
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-21 13:55:02 +0530

    LESSON 6 : removed CORS error by activating the plugin

--- Lesson 6/src/components/Body.js
+++ Lesson 6/src/components/Body.js
@@ -39,8 +39,9 @@ const Body = () => {
 
   async function getRestaurants() {
     const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
+    // const json = await response.json();
     const json = await response.json();
-    console.log("JSON: "+json);
+    console.log(json);
   }
    
   // SUMMARY:
