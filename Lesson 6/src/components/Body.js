e5c8cddcea1c4528b332d229cfc3159cb3c787c3
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-21 14:35:26 +0530

    LESSON 6 : using optional chaining while rendering data

--- Lesson 6/src/components/Body.js
+++ Lesson 6/src/components/Body.js
@@ -40,7 +40,7 @@ const Body = () => {
     const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
     // const json = await response.json();
     const json = await response.json();
-    const data = json.data.cards[2].data.data.cards;
+    const data = json?.data?.cards[2]?.data?.data?.cards;
     console.log(data);
     setFilteredRestaurantList(data);
   }
