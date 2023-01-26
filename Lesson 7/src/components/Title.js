c45b46c0201a10e2db5137bb86f037ee4a034225
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-01-26 10:12:10 +0530

    LESSON 7 : using a static image for rendering

--- Lesson 7/src/components/Title.js
+++ Lesson 7/src/components/Title.js
@@ -1,10 +1,12 @@
+import Logo from '../assets/images/foodVilla.png'
+
 const Title = () => {
   return (
     <a href="/">
       <img
         className="logo"
         alt="logo"
-        src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+        src={Logo}
       />
     </a>
   );
